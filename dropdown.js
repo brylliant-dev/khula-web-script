1338f881e5de01afd6e1c3c9406f7a1c8ba3a6d2
Brylliant Dev <153876117+brylliant-dev@users.noreply.github.com>
2024-12-03 19:38:03 +0800

    chore(): add array index

--- dropdown.js
+++ dropdown.js
@@ -249,7 +249,7 @@ $(document).ready(function () {
             if (statusByKey.length === 0) {
                 ddl.find('.w-dropdown-toggle').css('cursor', 'auto');
                 ddl.clone().insertBefore(ddl).remove();
-                console.log('--this is ddl', ddl)
+                console.log('--this is ddl', ddl[0])
                 return;
             }
 
